f6036f2370e45a4f5529b2e1c8d1f2a28c2d68a4
charitha <chariya.g@gmail.com>
2022-05-30 12:18:21 +1000

    intial commit

---
+++ register-student/src/test/java/com/studentregistrationapi/registerstudent/RegisterStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentregistrationapi.registerstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
